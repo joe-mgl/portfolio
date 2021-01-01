3032b7d0dbfbc5e16151cf8dbd6ddd63e7903346
joe-mgl <otepmigue@gmail.com>
2021-01-01 08:25:38 -0800

    creating gh repository for personal portfolio

--- src/mock/data.js
+++ src/mock/data.js
@@ -9,49 +9,54 @@ export const headData = {
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
+  title: 'Hello, my name is',
+  name: 'Joseph.',
+  subtitle: 'I am an aspiring Software Developer.',
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne:
+    'Programming has always been my Achilles’ heel and an area I want to explore but have been putting off for the longest time. Finally in July 2020, I purchased an online course to pursue my journey into the coding world. I started learning Python, Data Science and Machine Learning. I followed this up with web development courses focusing on the front-end using JavaScript and ReactJS, with a little bit of backend and database implementation.',
+  paragraphTwo:
+    'Prior to this, I spent that last twelve years working as an Electronics Technician in the oilfield services sector. The role has allowed me to gain various skills and competencies that have to do with repair and maintenance of different electronic tools, training and mentoring junior colleagues. It also gave me the opportunity to work outside of my home country, spending a good bit of time in the United Arab Emirates and Malaysia., where I met wonderful people from different backgrounds and cultures.',
+  paragraphThree:
+    'I recently moved to Long Beach, California where I am planning to launch my career as a Software Developer. I would say that I am still fairly new into coding but I am very eager to learn the ropes and be proficient in this field.',
+  resume: '', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'smartdetek.jpg',
+    title: 'Smart-Detek',
+    info: 'Smart-Detek is an image recognition application utilizing Machine Learning API.',
+    info2:
+      'This is a fullstack application developed using JavaScript, ReactJS, Express.JS, and PostgreSQL. Currently deployed using Heroku.',
+    url: 'https://smart-detek.herokuapp.com',
+    repo: 'https://github.com/joe-mgl/face-detection', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'retazo.jpg',
+    title: 'Retazo Clothing CA',
+    info: 'Retazo Clothing CA is an online, e-commerce application similar to Shopify.',
+    info2:
+      'This is a fullstack application utilizing ReactJS, Redux, React Hooks, Firebase, and Stripe. Currently under development; initial deployment using Heroku.',
+    url: 'https://retazo-ca.herokuapp.com',
+    repo: 'https://github.com/joe-mgl/retazo-clothing', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'robofriends.png',
+    title: 'Robofriends',
+    info: 'This is a simple web application created using ReactJS with API implementation.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://joe-mgl.github.io/robofriends/',
+    repo: 'https://github.com/joe-mgl/robofriends', // if no repo, the button will not show up
   },
 ];
 
@@ -59,7 +64,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'josephmiguel02@gmail.com',
 };
 
 // FOOTER DATA
@@ -67,23 +72,13 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/josephmiguel',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/joe-mgl',
     },
   ],
 };
